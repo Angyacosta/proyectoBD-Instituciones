83d467d2638b2087ee3b9eba2f434fb4dca84803
Angyacosta <adacosta.parrado@unillanos.edu.co>
2024-11-08 16:12:16 -0500

    Update conexionDB.php

--- conexionDB.php
+++ conexionDB.php
@@ -1,10 +1,8 @@
 <?php
 $host = "localhost";
 $dbname = "instituciones";
-$user = "user_inst";
-$password = "instituciones";
-
-//el usario deberia ser potsgres y su contrseña postgres eso luego lo cambiamos
+$user = "postgres";
+$password = "postgres";
 try {
 $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -12,3 +10,4 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo "Error: " . $e->getMessage();
 exit;
 }
+    
