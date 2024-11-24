e878aec82ab22bb0d1fbf94bd9fb430a8fd7351c
Angy <adacosta.parrado@unillanos.edu.co>
2024-11-24 11:30:20 -0500

    Update FuncionesSQL.sql

--- FuncionesSQL.sql
+++ FuncionesSQL.sql
@@ -93,11 +93,6 @@ $$ LANGUAGE plpgsql;
 
 
 
-
-f
-
-
-
 CREATE OR REPLACE FUNCTION busqueda(nombre_institucion text, codigo_institucion integer)
 RETURNS TABLE (nomb_inst text, cod_inst integer, cod_ies_padre integer, nomb_sector varchar, nomb_academ varchar, nomb_depto varchar, nomb_munic varchar, nomb_estado varchar, programas_vigente integer, programas_convenio integer, acreditada boolean) AS $$
 BEGIN
@@ -120,3 +115,69 @@ BEGIN
         (codigo_institucion IS NULL OR ipm.cod_inst = codigo_institucion);
 END;
 $$ LANGUAGE plpgsql;
+
+
+-----funciones de eliminacion, creacion y actuaizacion ---
+
+
+---reorganizara los codigos despues de su eliminacion 
+
+create or replace function reajustar_cod()
+returns trigger as $$
+begin
+update directivos 
+SET cod_directivo = cod_directivo -1
+where cod_directivo> OLD.cod_directivo;
+return OLD;
+END;
+$$ LANGUAGE  plpgsql;
+
+CREATE TRIGGER trigger_reajustar_cod
+AFTER DELETE ON directivos
+FOR EACH ROW
+EXECUTE FUNCTION reajustar_cod();
+
+CREATE OR REPLACE FUNCTION asignar_cod_faltante()
+RETURNS TRIGGER AS $$
+DECLARE
+    menor_cod_disponible INT;
+BEGIN
+    -- Encuentra el menor código faltante
+    SELECT min(cod_directivo + 1) 
+    INTO menor_cod_disponible
+    FROM directivos
+    WHERE (cod_directivo + 1) NOT IN (SELECT cod_directivo FROM directivos);
+
+    -- Si existe un código faltante, úsalo; si no, utiliza la secuencia por defecto
+    IF menor_cod_disponible IS NOT NULL THEN
+        NEW.cod_directivo = menor_cod_disponible;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Crear el trigger
+CREATE TRIGGER trigger_asignar_cod
+BEFORE INSERT ON directivos
+FOR EACH ROW
+EXECUTE FUNCTION asignar_cod_faltante();
+
+
+--tiggers
+CREATE OR REPLACE FUNCTION convertir_a_mayuscula()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.nomb_directivo := UPPER(NEW.nomb_directivo);
+    NEW.apell_directivo := UPPER(NEW.apell_directivo);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER before_insert_directivos
+BEFORE INSERT OR UPDATE ON directivos
+FOR EACH ROW
+EXECUTE FUNCTION convertir_a_mayuscula();
+
+--crearen mayusculas los nombres 
+
